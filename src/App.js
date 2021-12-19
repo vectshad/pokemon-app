b6933793f38591933323337285eb985633a54a58
ekasunandika <ekasunandikaa@gmail.com>
2021-12-20 01:03:49 +0700

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useQuery } from '@apollo/client';
+import { GET_POKEMON_DETAIL } from './graphql/pokemon';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
+import PokemonList from './components/PokemonList';
+import PokemonDetail from './components/PokemonDetail';
+import MyPokemonList from './components/MyPokemonList';
+
 import './App.css';
 
+
+const gqlVariables = {
+  name: 'bulbasaur',
+  // limit: 20,
+  // offset: 0,
+};
+
 function App() {
+  const { loading, error, data } = useQuery(GET_POKEMON_DETAIL, {
+    variables: gqlVariables,
+  });
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  console.log('Response from server', data.pokemon);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          {data.pokemon.name} */}
+        {/* </p> */}
+        {/* <ul>
+        // {data.pokemons.results.map((pokemon) => 
+        //     <li>
+        //       {pokemon.name}
+        //     </li>
+        //   )}
+        // </ul> */}
+      {/* </header> */}
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<PokemonList/>}/>
+          <Route path='/detail' element={<PokemonDetail/>}/>
+          <Route path='/list' element={<MyPokemonList/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
