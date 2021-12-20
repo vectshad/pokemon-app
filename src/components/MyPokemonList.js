cb73c7ba2ba9a3fc82c731b35ef076a18400d3ec
ekasunandika <ekasunandikaa@gmail.com>
2021-12-20 22:38:10 +0700

    use context for list

--- src/components/MyPokemonList.js
+++ src/components/MyPokemonList.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import '../styles/PokemonList.css';
 import { Link } from 'react-router-dom';
+import { PokemonContext } from "../contexts/PokemonContext";
 
-function MyPokemonList (props) {
+function MyPokemonList () {
+    const pokemons = useContext(PokemonContext);
+
+    // console.log(pokemons);
     return (
         <div className="PokemonList">
             <h2>My Pokemon</h2>
             <div className="Container">
-                {props.pokemons.map((pokemon) => 
-                    <div className="Pokemon">
+                {pokemons.pokemons.length === 0 &&
+                    <h3>No Pokemon !!!</h3>
+                }
+                {pokemons.pokemons.map((pokemon, index) => 
+                    <div key={index} className="Pokemon">
                         <Link to={{pathname: `/detail/${pokemon.name}`}}>
-                            <img src={pokemon.image} alt="pokemon"/>
+                            <img src={pokemon.sprites.front_default} alt="pokemon"/>
                         </Link>
                         <p>{pokemon.name}</p>
                     </div>
