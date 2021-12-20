3fb307830a0ee33b7e5dc3eb15e3d299012b5e8b
ekasunandika <ekasunandikaa@gmail.com>
2021-12-20 22:38:58 +0700

    add comment

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -3,13 +3,12 @@ import '../styles/PokemonList.css';
 import { Link } from 'react-router-dom';
 
 function PokemonList (props) {
-    console.log(props.pokemons[0])
     return(
         <div className="PokemonList">
             <h2>Pokemon List</h2>
             <div className="Container">
-                {props.pokemons.map((pokemon) => 
-                    <div className="Pokemon">
+                {props.pokemons.map((pokemon, index) => 
+                    <div key={index} className="Pokemon">
                         <Link to={{pathname: `/detail/${pokemon.name}`}}>
                             <img src={pokemon.image} alt="pokemon"/>
                         </Link>
